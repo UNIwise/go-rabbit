04c25030aab835620571cbffc89279a61515c320
Jonas Tranberg <jonastranberg93@gmail.com>
2020-06-29 15:25:45 +0200

    Fixed package naming

--- rabbit.go
+++ rabbit.go
@@ -1,14 +1,16 @@
-package main
+package rabbit
 
 import (
 	"github.com/uniwise/go-rabbit/client"
 	"github.com/uniwise/go-rabbit/config"
 )
 
+// New returns a new RabbitMQ client with the provided configuration
 func New(conf *config.Config) (client.RabbitMQClient, error) {
 	return client.New(conf)
 }
 
+// NewEnvClient returns a new RabbitMQ client with the configuration parsed from environment variables
 func NewEnvClient() (client.RabbitMQClient, error) {
 	conf, err := config.NewEnvConfig()
 	if err != nil {
