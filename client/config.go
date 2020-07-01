0fbc36ea3879727bf572e852c412e273ea3885c5
Jonas Tranberg <jonastranberg93@gmail.com>
2020-07-01 13:27:25 +0200

    Added auto .env loading

--- client/config.go
+++ client/config.go
@@ -1,6 +1,10 @@
 package client
 
-import "github.com/kelseyhightower/envconfig"
+import (
+	// Used to load .env files for environment variables
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/kelseyhightower/envconfig"
+)
 
 // Config contains preferences needed for a RabbitMQ amqp client connection
 type Config struct {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/isayme/go-amqp-reconnect v0.0.0-20180930040740-e71660afb5ca
+	github.com/joho/godotenv v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1
 	github.com/streadway/amqp v1.0.0
